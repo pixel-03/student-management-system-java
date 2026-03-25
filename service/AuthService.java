229b30e0e7e74dba19d9659acd7bce3add375a96
pixel-03 <mohammednoor00012@gmail.com>
2026-03-25 19:27:11 +0530

    Refactor AuthService to use a user map for authentication

--- service/AuthService.java
+++ service/AuthService.java
@@ -1,11 +1,29 @@
 package service;
 
+import java.util.HashMap;
+
 public class AuthService {
 
-    private final String USERNAME = "admin";
-    private final String PASSWORD = "1234";
+    private HashMap<String, String> users = new HashMap<>();
+
+    public AuthService() {
+        // Default user
+        users.put("admin", "1234");
+    }
 
     public boolean login(String username, String password) {
-        return USERNAME.equals(username) && PASSWORD.equals(password);
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void register(String username, String password) {
+        if (users.containsKey(username)) {
+            System.out.println("User already exists!");
+        } else {
+            users.put(username, password);
+            System.out.println("User registered successfully!");
+        }
     }
 }
